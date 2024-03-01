1c3e6b75535cf86f2ed3aba613a5fba574addf55
Alina <info@alina-nerlich.com>
2024-03-01 12:51:28 +0100

    add Loading

--- src/components/APOD_API.jsx
+++ src/components/APOD_API.jsx
@@ -5,38 +5,42 @@ import { useState, useEffect } from "react";
 export default function API({ date }) {
   const NASA_url = "https://api.nasa.gov/";
   const API_KEY = "Q13qOhqlmIwXBrv56PROSrbeDYdw7D4A0OFRLKic";
-  const [title, setTitle] = useState("");
-  const [image, setImage] = useState("");
-  const [copyright, setCopyright] = useState("");
-  const [explanation, setExplanation] = useState("");
+  const [data, setData] = useState();
 
   useEffect(() => {
     async function fetchAPODAPI() {
       await fetch(`${NASA_url}planetary/apod?date=${date}&api_key=${API_KEY}`)
         .then((response) => response.json())
         .then((data) => {
-          setTitle(data.title),
-            setImage(data.url),
-            setCopyright(data.copyright);
-          setExplanation(data.explanation);
+         setData(data)
         });
     }
-    fetchAPODAPI();
+    setTimeout(()=>{
+      fetchAPODAPI();
+    }, 2000)
+
   }, [date]);
 
   return (
-    <div>
+    <>
+    {data ? (
+      <div>
       <h1>Astronomical Picture of the Day</h1>
-      <h2>{title}</h2>
+      <h2>{data?.title}</h2>
       <h3>This Image corresponds to the date {date}</h3>
-
-      {image.indexOf("youtube") > -1 ? (
-        <iframe width="420" height="315" src={image}></iframe>
+      
+      {data?.url.indexOf("youtube") > -1 ? (
+        <iframe width="420" height="315" src={data?.url}></iframe>
       ) : (
-        <img src={image} alt={title} width="420" height="315"/>
+        <img src={data?.url} alt={data?.title} width="420" height="315"/>
       )}
-      <p>{copyright}</p>
-      <p>{explanation}</p>
+      <p>{data?.copyright}</p>
+      <p>{data?.explanation}</p>
     </div>
+    ) : (
+      <div>LOADING...</div>
+    )}
+    </>
+
   );
 }
--- src/components/MARS_ROVER_API.jsx
+++ src/components/MARS_ROVER_API.jsx
@@ -5,41 +5,44 @@ import { useState, useEffect } from "react";
 export default function MARS_ROVER_API({ date }) {
   const NASA_url = "https://api.nasa.gov/mars-photos/api/v1/";
   const API_KEY = "Q13qOhqlmIwXBrv56PROSrbeDYdw7D4A0OFRLKic";
-  const [camera, setCamera] = useState("");
-  const [image, setImage] = useState("");
-  const [rover, setRover] = useState("");
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchMarsRover() {
+      setLoading(true);
       await fetch(
         `${NASA_url}rovers/curiosity/photos?earth_date=${date}&api_key=${API_KEY}`
       )
         .then((response) => response.json())
         .then((data) => {
-          setCamera(data.photos[0].camera.full_name),
-            setImage(data.photos[0].img_src),
-            setRover(data.photos[0].rover.name);
+          setData(data.photos[0]);
+          setLoading(false);
+
         });
     }
-    fetchMarsRover();
+    setTimeout(() => {
+      fetchMarsRover();
+    }, 5000);
   }, [date]);
 
   return (
     <div>
-      {image ? (
+      {loading ? <h2>Loading</h2> : null}
+      {data ? (
         <div>
           <h1>Mars Rover Picture of the Day</h1>
-          <h2>CAMERA:{camera}</h2>
+          <h2>CAMERA:{data?.camera.full_name}</h2>
           <h3>This Image corresponds to the date {date}</h3>
-          {image.indexOf("youtube") > -1 ? (
-            <iframe width="420" height="315" src={image}></iframe>
+          {data?.img_src.indexOf("youtube") > -1 ? (
+            <iframe width="420" height="315" src={data?.img_src}></iframe>
           ) : (
-            <img src={image} width="420" height="315"/>
+            <img src={data?.img_src} width="420" height="315" />
           )}
-          <p>Rover: {rover}</p>
+          <p>Rover: {data?.rover.name}</p>
         </div>
       ) : (
-        <h2>No pics on this day - try another date</h2>
+        <h2>No pics today</h2>
       )}
     </div>
   );
